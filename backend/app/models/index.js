f8353829899f3a6d170ced886266cd9cccd7e7e9
Ubuntu <ubuntu@ip-172-31-47-16.ap-south-1.compute.internal>
2025-11-27 19:24:30 +0000

    first commit

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,11 +1,17 @@
-const dbConfig = require("../config/db.config.js");
-
 const mongoose = require("mongoose");
+
+// Use native Promises
 mongoose.Promise = global.Promise;
 
+// Build DB object
 const db = {};
+
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = process.env.MONGODB_URI || "mongodb://mongo:27017/tutorials_db";
+
+// Load models
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
+// Export single db object
 module.exports = db;
+
